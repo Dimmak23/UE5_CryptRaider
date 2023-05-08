575b62f5f6df4d1a864dccb795d3af5b59de92a3
Dmytro Kovryzhenko <onemoremagistr@gmail.com>
2023-05-09 00:40:17 +0300

    moving doors

---
+++ Source/CryptRaider/Mover.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Mover.h"
+
+#include "Math/UnrealMathUtility.h"
+
+// Sets default values for this component's properties
+UMover::UMover()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these
+	// features off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+// Called when the game starts
+void UMover::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	OriginalLocation = GetOwner()->GetActorLocation();
+	TargetLocation = OriginalLocation + MoveOffset;
+	Speed = FVector::Distance(OriginalLocation, TargetLocation) / MoveTime;
+}
+
+// Called every frame
+void UMover::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// FVector TargetLocation = OriginalLocation + MoveOffset;
+	// float Speed = FVector::Distance(OriginalLocation, TargetLocation) / MoveTime;
+
+	FVector CurrentLocation = GetOwner()->GetActorLocation();
+
+	FVector NewLocation = FMath::VInterpConstantTo(CurrentLocation, TargetLocation, DeltaTime, Speed);
+
+	GetOwner()->SetActorLocation(NewLocation);
+}
