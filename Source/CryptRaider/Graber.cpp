221c7a001b61fab39e2d3a4160b808561d021e7c
Dmytro Kovryzhenko <onemoremagistr@gmail.com>
2023-05-10 00:45:38 +0300

    methods with debugging spheres and line commented

--- Source/CryptRaider/Graber.cpp
+++ Source/CryptRaider/Graber.cpp
@@ -13,8 +13,6 @@ UGraber::UGraber()
 	PrimaryComponentTick.bCanEverTick = true;
 
 	// ...
-
-	MaxGrabDistance = 400.0f;
 }
 
 // Called when the game starts
@@ -23,6 +21,19 @@ void UGraber::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
+	// if (PhysicsHandle != nullptr)
+	// {
+	// 	UE_LOG(LogTemp, Display, TEXT("Found physics hangler: %s..."), *(PhysicsHandle->GetName()));
+	// }
+	// else
+	// {
+	// 	UE_LOG(LogTemp, Warning, TEXT("No physics hangler were found!"));
+	// }
+	if (MaxGrabDistance == 0.0f) MaxGrabDistance = 400.0f;
+	if (GrabRadius == 0.0f) GrabRadius = 20.f;
+	if (HoldDistance == 0.0f) HoldDistance = 50.0f;
+
+	PhysicsHandle = GetPhysicsHandle();
 }
 
 // Called every frame
@@ -31,10 +42,101 @@ void UGraber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponen
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
-	StartPosition = GetComponentLocation();
-	DestPosition = StartPosition + GetForwardVector() * MaxGrabDistance;
+	if (PhysicsHandle == nullptr) return;
+
+	FVector TargetLocation = GetComponentLocation() + GetForwardVector() * HoldDistance;
+	PhysicsHandle->SetTargetLocationAndRotation(TargetLocation, GetComponentRotation());
+}
+
+void UGraber::GrabStuff()
+{
+	if (PhysicsHandle == nullptr) return;
+
+	StartPosition = GetComponentLocation();									// current position of the player
+	DestPosition = StartPosition + GetForwardVector() * MaxGrabDistance;	// destiantion vector from player
 
 	// UE_LOG(LogTemp, Display, TEXT("Grabber Rotation: %s"), *(GetComponentRotation().ToCompactString()));
 	// UE_LOG(LogTemp, Display, TEXT("Current Time Is: %f"), GetWorld()->TimeSeconds);
-	DrawDebugLine(this->GetWorld(), StartPosition, DestPosition, FColor::Red);
+	// DrawDebugLine(this->GetWorld(), StartPosition, DestPosition, FColor::Red);
+	// DrawDebugSphere(								   //
+	// 	this->GetWorld(), DestPosition, GrabRadius,	   //
+	// 	10, FColor::Red, false, 5					   //
+	// );												   // End point of our grabbing stick
+
+	HitSphere = FCollisionShape::MakeSphere(GrabRadius);
+
+	HasHit = this->GetWorld()->SweepSingleByChannel(	//
+		HitResult,										//
+		StartPosition,									//
+		DestPosition,									//
+		FQuat::Identity,								//
+		ECC_GameTraceChannel2,							//
+		HitSphere										//
+	);
+	// FQuat::Identity --> no rotation
+	// ECollisionChannel TraceChannel --> {project_folder}/Config/DefaultEngine.ini --> Channel=ECC_GameTraceChannel2,
+	// for: Name="GraberChannel"
+	// +DefaultChannelResponses=(Channel=ECC_GameTraceChannel2, DefaultResponse=ECR_Ignore, bTraceType=True,
+	// bStaticObject=False, Name="GraberChannel")
+
+	if (HasHit)
+	{
+		// Actually grab something
+		PhysicsHandle->GrabComponentAtLocationWithRotation(	   //
+			HitResult.GetComponent(),						   //
+			NAME_None,										   //
+			HitResult.ImpactPoint,							   //
+			GetComponentRotation()							   //
+		);
+
+		// // Where is the component?
+		// DrawDebugSphere(this->GetWorld(),														 //
+		// 				HitResult.Component.Get()->GetRelativeLocation() + FVector(0, 0, 50),	 //
+		// 				50, 20, FColor::Green, false, 5											 //
+		// );	  // Desirable gravable object
+		// // Location in a world space where the moving shape would end up against the impacted
+		// // object, if there is a hit
+		// DrawDebugSphere(this->GetWorld(),					//
+		// 				HitResult.Location,					//
+		// 				20, 10, FColor::Purple, false, 5	//
+		// );
+		// // Where we hit him?
+		// DrawDebugSphere(this->GetWorld(),					//
+		// 				HitResult.ImpactPoint,				//
+		// 				10, 10, FColor::Yellow, false, 5	//
+		// );													// Center of the sphere is the hit point
+
+		// UE_LOG(														 //
+		// 	LogTemp, Display,										 //
+		// 	TEXT("Current Time is: %f, hiting the actor: %s..."),	 //
+		// 	GetWorld()->TimeSeconds,								 // get time
+		// 	*(HitResult.GetActor()->GetActorNameOrLabel())			 // get name
+		// );
+	}
+	else
+	{
+		UE_LOG(											//
+			LogTemp, Display,							//
+			TEXT("Current Time is: %f, no hitting"),	//
+			GetWorld()->TimeSeconds						// get time
+		);
+	}
+}
+
+void UGraber::ReleaseStuff() { UE_LOG(LogTemp, Display, TEXT("Grabber have released object...")); }
+
+UPhysicsHandleComponent* UGraber::GetPhysicsHandle() const
+{
+	UPhysicsHandleComponent* Result = this->GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if (Result == nullptr)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Grabber requires a UPhysicsHandleComponent."));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Display, TEXT("Actually found a physics handler: %s..."), *(PhysicsHandle->GetName()));
+	}
+	Result->RegisterComponent();
+
+	return Result;
 }
--- Source/CryptRaider/Graber.h
+++ Source/CryptRaider/Graber.h
@@ -4,6 +4,9 @@
 
 #include "Components/SceneComponent.h"
 #include "CoreMinimal.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+
+// should be the last header always!
 #include "Graber.generated.h"
 
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
@@ -24,9 +27,33 @@ public:
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
 							   FActorComponentTickFunction* ThisTickFunction) override;
 
+	UFUNCTION(BlueprintCallable)
+	void GrabStuff();
+
+	UFUNCTION(BlueprintCallable)
+	void ReleaseStuff();
+
 private:
 	// utilities
 	FVector StartPosition;
 	FVector DestPosition;
+
+	// our virtual stick, that we use for grabing
+	UPROPERTY(EditAnywhere)
 	float MaxGrabDistance{};
+
+	UPROPERTY(EditAnywhere)
+	float GrabRadius{};
+
+	// but we hold object closer to us
+	UPROPERTY(EditAnywhere)
+	float HoldDistance{};
+
+	FCollisionShape HitSphere;
+	FHitResult HitResult;
+	bool HasHit{ false };
+	// physics
+	UPhysicsHandleComponent* PhysicsHandle;
+
+	UPhysicsHandleComponent* GetPhysicsHandle() const;
 };
