25b114f2ec0171a07461bb8189dc3c9580fc2b98
Dmytro Kovryzhenko <onemoremagistr@gmail.com>
2023-11-12 02:05:47 +0200

    added new background sound

---
+++ Scripts/DeleteUnusedAssets.py
@@ -0,0 +1,29 @@
+#! This script was tested: it worked
+
+import unreal
+
+workingPath = "/Game/"
+
+@unreal.uclass()
+class GetEditorAssetLibrary(unreal.EditorAssetLibrary):
+    pass
+
+editorAssetLib = GetEditorAssetLibrary();
+
+allAssets = editorAssetLib.list_assets(workingPath, True, False)
+
+processingAssetPath = ""
+allAssetsCount = len(allAssets)
+if ( allAssetsCount > 0):
+    with unreal.ScopedSlowTask(allAssetsCount, processingAssetPath) as slowTask:
+        slowTask.make_dialog(True)
+        for asset in allAssets:
+            processingAssetPath = asset
+            deps = editorAssetLib.find_package_referencers_for_asset(asset, False)
+            if (len(deps) <= 0):
+            # if (len(deps) == 0): # Consider to chage this line to comparing with '0', no need to check negative lenght
+                print (">>> Deleting >>> %s" % asset)
+                editorAssetLib.delete_asset(asset)
+            if slowTask.should_cancel():
+                break
+            slowTask.enter_progress_frame(1, processingAssetPath)
